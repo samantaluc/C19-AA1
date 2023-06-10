f7f61077735e0ada34df45e924e8ef8d54f0df66
Samanta Adriane Sedlaczek Luchoski <49082205+samantaluc@users.noreply.github.com>
2023-06-10 19:02:38 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,65 +15,62 @@ function preload(){
 
 function setup(){
   createCanvas(600,600);
-  spookySound.loop();
+  spookySound.loop(); //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
   tower = createSprite(300,300);
   tower.addImage("tower",towerImg);
   tower.velocityY = 1;
   
-  doorsGroup = new Group();
-  climbersGroup = new Group();
-  invisibleBlockGroup = new Group();
+  doorsGroup = new Group(); //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
+  climbersGroup = new Group(); //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
+  invisibleBlockGroup = new Group(); //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
   
-  ghost = createSprite(200,200,50,50);
-  ghost.scale = 0.3;
-  ghost.addImage("ghost", ghostImg);
+  ghost = createSprite(200,200,50,50); //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
+  ghost.scale = 0.3; //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
+  ghost.addImage("ghost", ghostImg); //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
 }
 
 function draw(){
   background(0);
-  if (gameState === "play") {
-    if(keyDown("left_arrow")){
-      ghost.x = ghost.x - 3;
-    }
+  if (gameState === "play") { //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
+    if(keyDown("left_arrow")){ //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
+      ghost.x = ghost.x - 3; //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
+    } //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
     
-    if(keyDown("right_arrow")){
-      ghost.x = ghost.x + 3;
-    }
+    if(keyDown("right_arrow")){ //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
+      ghost.x = ghost.x + 3; //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
+    } //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
     
-    if(keyDown("space")){
-      ghost.velocityY = -10;
-    }
+    if(keyDown("space")){ //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
+      ghost.velocityY = -10; //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
+    } //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
     
-    ghost.velocityY = ghost.velocityY + 0.8
+    ghost.velocityY = ghost.velocityY + 0.8 //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
     
     if(tower.y > 400){
       tower.y = 300
     }
-    spawnDoors();
-
-    
-    //climbersGroup.collide(ghost);
-    if(climbersGroup.isTouching(ghost)){
-      ghost.velocityY = 0;
-    }
-    if(invisibleBlockGroup.isTouching(ghost) || ghost.y > 600){
-      ghost.destroy();
-      gameState = "end"
-    }
-    
+    spawnDoors(); //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
+ 
+    if(climbersGroup.isTouching(ghost)){ //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
+      ghost.velocityY = 0; //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
+    } //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
+    if(invisibleBlockGroup.isTouching(ghost) || ghost.y > 600){ //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
+      ghost.destroy(); //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
+      gameState = "end" //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
+    } //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
     drawSprites();
-  }
+  } //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
   
-  if (gameState === "end"){
-    stroke("yellow");
-    fill("yellow");
-    textSize(30);
-    text("Fim de Jogo", 230,250)
-  }
+  if (gameState === "end"){ //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
+    stroke("yellow"); //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
+    fill("yellow"); //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
+    textSize(30); //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
+    text("Fim de Jogo", 230,250) //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
+  } //adicionar <<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 }
 
-function spawnDoors() {
+function spawnDoors() {  //adicionar toda a função <<<<<<<<<<<<<<<<<<<<<<<<<<<
   //escreva aqui o código para gerar as portas na torre
   if (frameCount % 240 === 0) {
     var door = createSprite(200, -50);
@@ -101,7 +98,6 @@ function spawnDoors() {
     climber.lifetime = 800;
     invisibleBlock.lifetime = 800;
 
-    
     //adicione cada porta ao grupo
     doorsGroup.add(door);
     invisibleBlock.debug = true;
